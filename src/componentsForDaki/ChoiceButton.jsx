1e00be9e88afe09d83b70962bf17ce04b1380f01
Zaki <ZakiN>
2023-08-19 19:56:53 +0200

    sada jos malo

---
+++ src/componentsForDaki/ChoiceButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChoiceButton() {
+  return (
+    <div>
+      <h3>select button to show choice</h3>
+    </div>
+  )
+}
---
+++ src/pages/Daki.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Daki() {
+  return (
+    <div>Daki</div>
+  )
+}
