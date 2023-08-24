9228795963c28a02560f8bc3dd9a3030de6d054c
Dinonononono1989 <andriano.djordjevic@yahoo.com>
2023-08-24 21:40:23 +0200

    changes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,7 +6,9 @@ export default function Counter() {
         setCount(count +1)
     };
     const minusOne =()=>{
-        setCount(count -1)
+      if (count > 0) {
+        setCount(count - 1);
+      }
     }
 
   return (
--- src/components/TableMemorijaZaRačunare.jsx
+++ src/components/TableMemorijaZaRačunare.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import memorijaZaRačunare from './podaci'
 import Counter from './Counter'
+import { useState } from 'react'
 
 export default function TableMemorijaZaRačunare() {
+  const [istina, setIstina] = useState(false);
+  const addCounter = () => {
+    console.log("kliknuto");
+    setIstina(true)
+    }
+  
   return (
     <div><h3>memorija za računare</h3>
-
 <table>
   <tr>
     <th>Ime memorije</th>
@@ -15,19 +21,19 @@ export default function TableMemorijaZaRačunare() {
   </tr>
   <tr>
     <td>{memorijaZaRačunare[0].imeMemorije}</td>
-    <td>{memorijaZaRačunare[0].cena1} <Counter countValue={0}/></td>
-    <td>{memorijaZaRačunare[0].cena2} </td>
+    <td onClick={addCounter}>{memorijaZaRačunare[0].cena1} {istina ? <Counter /> : "" }</td>
+    <td onClick={addCounter}>{memorijaZaRačunare[0].cena2} {istina ? <Counter /> : "" }</td>
     <td></td>
   </tr>
   <tr>
     <td>{memorijaZaRačunare[1].imeMemorije}</td>
-    <td>{memorijaZaRačunare[1].cena1}</td>
-    <td>{memorijaZaRačunare[1].cena2}</td>
+    <td onClick={addCounter}>{memorijaZaRačunare[1].cena1} {istina ? <Counter /> : "" }</td>
+    <td onClick={addCounter}>{memorijaZaRačunare[1].cena2} {istina ? <Counter /> : "" }</td>
   </tr>
   <tr>
     <td>{memorijaZaRačunare[2].imeMemorije}</td>
-    <td>{memorijaZaRačunare[2].cena1}</td>
-    <td>{memorijaZaRačunare[2].cena2}</td>
+    <td onClick={addCounter}>{memorijaZaRačunare[2].cena1} {istina ? <Counter /> : "" }</td>
+    <td onClick={addCounter}>{memorijaZaRačunare[2].cena2}{istina ? <Counter /> : "" }</td>
   </tr>
 </table>
     </div>
